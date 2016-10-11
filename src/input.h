ac060b16c30aab15e75595292456126c70bcfaf2
Justin Siekmann <Justin Siekmann>
2016-10-10 20:47:09 -0700

    goop

---
+++ src/input.h
@@ -0,0 +1,8 @@
+/****************************************
+Authors: Justin Siekmann, Anthony Ciancio
+NetIDs : jsiekmann, shjay
+Date : 25 October, 2016
+Assignment #2
+
+Input.h
+*****************************************/
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/****************************************
+Authors: Justin Siekmann, Anthony Ciancio
+NetIDs : jsiekmann, shjay
+Date : 25 October, 2016
+Assignment #2
+
+main.cpp
+*****************************************/
+
+int main(){
+
+}
